63a2389852e370048d665fc0320717444d2e2fee
Zaharie Oana <zaharieoanadenisa@yahoo.com>
2024-04-20 20:43:08 +0300

    del

--- Bookstore/src/main/java/com/example/Bookstore/constants/UserType.java
+++
@@ -1,6 +0,0 @@
-package com.example.Bookstore.constants;
-
-public enum UserType {
-    CLIENT,
-    ADMIN
-}
--- Bookstore/src/main/java/com/example/Bookstore/repository/SaleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Bookstore.repository;
-
-import com.example.Bookstore.model.Sale;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SaleRepository extends CrudRepository<Sale, Long> {
-}
